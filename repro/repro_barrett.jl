d2eebb3f8b65638b1802776857cfed9234c2eea9
Arapaima gigas <profe.paiche@gmail.com>
2022-02-24 14:40:14 +0100

    Barrett: Reproducing indeces

--- repro/repro_barrett.jl
+++ repro/repro_barrett.jl
@@ -1,7 +1,5 @@
 # --------------------------------------------------------------------------- #
-# Script to reproduce part of the research of Barrett 2017. 
-# Here only the pressure indexes are calculated to check if the results are
-# the same as on the reference.
+# Script to reproduce indeces proposed by Barrett 2017. 
 # --------------------------------------------------------------------------- #
 
 # =========================================================================== #
@@ -9,22 +7,51 @@
 # =========================================================================== #
 
 using NCDatasets
-using Plots
+using DataFrames
+using Dates
+using CSV
+# using UnicodePlots
 
 # =========================================================================== #
 # FUNCTIONS
 # =========================================================================== #
 
 """
-Calculation of the pressure index according to Barret 2017.
+    SPHIndex(pressure::Array{Float32, 3}, lat::Vector{Float32}; reference_pressure::Float64)
+
+Calculation of the South Pacific High (SPH) indeces according to Barret 2017.
+Requires the `pressure` field as a 2D array, a vector `lat` with all the latitudes,
+and optionally a `reference_pressure` that will be used to calculate the anomalies.
+
+Note: Initially, the index was created to work at montly level.
 """
-pressureIndex = function(pressure, lat, lon_size; reference_pressure = 1016.0)
+
+SPHIndex = function(pressure::Array{Float32, 3},
+                    lon::Vector{Float32},
+                    lat::Vector{Float32};
+                    reference_pressure::Float64 = 1016.0)
+    # Lon lat grid
+    lon_size = size(lon, 1)
+    lat_size = size(lat, 1)
+    lon_grid = repeat(lon, inner = (1, lat_size))
     lat_grid = repeat(lat', inner = (lon_size, 1))
-    lat_correction = cos.(lat_grid)
+    lat_correction = cos.(lat_grid .* pi ./ 180)
+    # Pressure calculations
     pressure_anomaly = pressure .- reference_pressure
     selected_grids = pressure_anomaly .> 0
-    pressure_index = sum(pressure_anomaly .* lat_correction .* selected_grids) / sum(lat_correction .* selected_grids)
-    return pressure_index
+    # Loop through time
+    ntime = size(pressure, 3)
+    pressure_index = zeros(ntime)
+    latitude_index = zeros(ntime)
+    longitude_index = zeros(ntime)
+    for i in 1:ntime
+        pressure_index[i] = sum(pressure_anomaly[:,:,i] .* lat_correction .* selected_grids[:,:,i]) / sum(lat_correction .* selected_grids[:,:,i])
+        latitude_index[i] = sum(pressure_anomaly[:,:,i] .* lat_grid .* lat_correction .* selected_grids[:,:,i]) / sum(pressure_anomaly[:,:,i] .* lat_correction .* selected_grids[:,:,i])
+        longitude_index[i] = sum(pressure_anomaly[:,:,i] .* lon_grid .* lat_correction .* selected_grids[:,:,i]) / sum(pressure_anomaly[:,:,i] .* lat_correction .* selected_grids[:,:,i])
+    end
+    # Return table
+    sph_index = DataFrame(pressure = pressure_index, lon = longitude_index, lat = latitude_index)
+    return sph_index
 end
 
 # =========================================================================== #
@@ -36,8 +63,9 @@ area = (160, 290, -45, -10)
 
 # Load the data
 slp_file = NCDataset("data/ncep-ncar_mslp_1948-2021-mon.nc");
+tim = slp_file["time"]
 lon = slp_file["lon"]
-lat = slp_file["lat"] # -> this shit is reveser order
+lat = slp_file["lat"]
 slp = slp_file["slp"]
 
 # Select region
@@ -47,5 +75,7 @@ lat = lat[lat_sel]
 lon = lon[lon_sel]
 slp_crop = slp[lon_sel, lat_sel, :]
 
-# Calculate the pressure index
-pressure_index = pressureIndex(slp_crop[:,:,1], lat, size(lon, 1))
+# Calculate the indeces
+sph_index = SPHIndex(slp_crop, lon, lat)
+sph_index.date = Date.(tim)
+CSV.write("data/sph_index.csv", sph_index)
